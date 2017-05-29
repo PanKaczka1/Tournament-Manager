aad04279ea695352cc3de8d8a8029a394c84bf8a
Paweł Jacewicz <pawel138@gmail.com>
2017-05-29 15:32:33 +0200

    Added Team, Referee, Match and derived classes

---
+++ Tournament-Manager/DodgeballMatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament_Manager
+{
+    class DodgeballMatch : Match
+    {
+        public DodgeballMatch(Team t1, Team t2, Referee referee)
+        {
+            team1 = t1;
+            team2 = t2;
+            referees = new Referee[1];
+            referees[0] = referee;
+        }
+        public override void Play(int points1, int points2)
+        {
+            if (points1 > points2)
+            {
+                team1.Points += this.WinningPoints();
+                team2.Points += this.LosingPoints();
+            } else
+            {
+                team1.Points += this.LosingPoints();
+                team2.Points += this.WinningPoints();
+            }
+        }
+        public override uint WinningPoints()
+        {
+            return 1;
+        }
+        public override uint LosingPoints()
+        {
+            return 0;
+        }
+    }
+}
