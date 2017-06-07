59e2c4b7db2a1294f1c951b8814afb0ef10ab5c4
Paweł Jacewicz <pawel138@gmail.com>
2017-06-07 12:07:58 +0200

    PlayMatchWindow

---
+++ Tournament-Manager/PlayMatchWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tournament_Manager
+{
+    /// <summary>
+    /// Interaction logic for PlayMatchWindow.xaml
+    /// </summary>
+    public partial class PlayMatchWindow : Window
+    {
+        private Match match;
+        public PlayMatchWindow(Object match, Disc discipline)
+        {
+            this.match = match as Match;
+            InitializeComponent();
+            FirstTeamLabel.Content = this.match.Team1.Name;
+            SecondTeamLabel.Content = this.match.Team2.Name;
+
+            FirstTeamScoresCB.Items.Add("0");
+            FirstTeamScoresCB.Items.Add("1");
+
+            SecondTeamScoresCB.Items.Add("0");
+            SecondTeamScoresCB.Items.Add("1");
+            if (discipline == Disc.Volleyball)
+            {
+                FirstTeamScoresCB.Items.Add("2");
+                FirstTeamScoresCB.Items.Add("3");
+
+                SecondTeamScoresCB.Items.Add("2");
+                SecondTeamScoresCB.Items.Add("3");
+            } else
+            {
+                SecondRefereeCB.Visibility = Visibility.Hidden;
+                ThirdRefereeCB.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
--- Tournament-Manager/Schedule.xaml.cs
+++ Tournament-Manager/Schedule.xaml.cs
@@ -20,26 +20,58 @@ namespace Tournament_Manager
     public partial class Schedule : Window
     {
         private Tournament tournament;
+        private Disc discipline;
         public Schedule(Tournament t, Disc discipline)
         {
             DataContext = this;
             this.tournament = t;
+            this.discipline = discipline;
             InitializeComponent();
-            volleyballScheduleLV.ItemsSource = tournament.ContestVolleyball.League.Matches;
 
-            switch (discipline)
+            switch (this.discipline)
             {
                 case Disc.Volleyball:
-                    
+                    ScheduleLV.ItemsSource = tournament.ContestVolleyball.League.Matches;
                     break;
                 case Disc.Dodgeball:
-                    
+                    ScheduleLV.ItemsSource = tournament.ContestDodgeball.League.Matches;
                     break;
                 case Disc.RopeDragging:
-                    
+                    ScheduleLV.ItemsSource = tournament.ContestRopeDragging.League.Matches;
                     break;
             }
         }
 
+        private void playBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Match m = sender as Match;
+            PlayMatchWindow pmw;
+            switch(discipline)
+            {
+                case Disc.Volleyball:
+                    pmw = new PlayMatchWindow(ScheduleLV.SelectedItem, Disc.Volleyball);
+                    pmw.Show();
+                    break;
+                case Disc.Dodgeball:
+                    pmw = new PlayMatchWindow(ScheduleLV.SelectedItem, Disc.Dodgeball);
+                    pmw.Show();
+                    break;
+                case Disc.RopeDragging:
+                    pmw = new PlayMatchWindow(ScheduleLV.SelectedItem, Disc.RopeDragging);
+                    pmw.Show();
+                    break;
+            }
+
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ScheduleLV_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            playBtn.IsEnabled = true;
+        }
     }
 }
